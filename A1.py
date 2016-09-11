67f9826f09b0cd61ff7239487c1377115dd588c4
Miguel Nava <mike311878@compute-linux1.cs.tamu.edu>
2016-09-11 01:55:52 -0500

    Began parsing and needs to parse lines correctly based on parameters needed

--- A1.py
+++ A1.py
@@ -1,13 +1,13 @@
 # Author: Miguel Nava
 # Assignment: Individual assignment: game leaderboard tracker
-class Player:
-	def f(self):
-		print "hello world"
-		return
-#AddPlayer <Player ID> <Player Name>
-	#Adds player to database with a positve integer identifier
-	#Name is given as a string, can contain special characters except double quotes
-def AddPlayer (playerID, playerName):
+import fileinput
+
+Player = {}
+Game = {}
+
+
+#FIXME make sure to convert playerID from string to integer at some point 
+def AddPlayer (playerID, playerName): 
     if isinstance(playerID, int): 			# checks if ID is an int
         if playerID >= 0:					# checks if ID is positive 
             print playerID					# need to add to database 
@@ -20,43 +20,87 @@ def AddPlayer (playerID, playerName):
         print "string contains \""
     return
 
-AddPlayer(1234,"miguel nava")
-Player.f
-	
 #AddGame <Game ID> <Game Name>
 	#Adds game to data base with a positive integer indentifier 
 	#Name is given as string, can contain special characters except double quotes 
-	
+def AddGame (gameID, gameName):
+		print gameID
+		print gameName
 #AddVictory <Game ID> <Victory ID> <Victory Name> <Victory Points>
 	#Add victory to the game denoted by the Game ID
 	#Victory ID is just to identify the victory 
 	#Victory name is given as string, cannot contain double quotes
 	#The points indicate what the victory is worth
+def AddVictory(gameID,victoryID, victoryName,victoryPoints):
+	print gameID
+	print victoryID
+	print victoryName
+	print victoryPoints
 	
 #Plays <Player ID> <Game ID> <Player IGN>
 	# 
 	#
+def Plays(playerID, gameID, playerIGN):
+	print "plays"
 #AddFriends <Player ID1> <Player ID2>
 	#
 	#
+def AddFriends(playerID1, playerID2):
+	print "friends"
 #WinVictory <Player ID> <Game ID> <Victory ID>
 	#
 	#
+def WinVictory(playerID, gameID, victoryID):
+	print "win victory"
 #FriendsWhoPlay <Player ID> <Game ID>
 	#
 	#
+def FriendsWhoPlay(playerID, gameID):
+	print "friends who play"
 #ComparePlayers <Player ID1> <Player ID2> <Game ID>
 	#
 	#
+def ComparePlayers(playerID1, playerID2, gameID):
+	print "comparing players"
 #SummarizePlayer <Player ID>
 	#
 	#
+def SummarizePlayer(playerID):
+	print "summarize player"
 #SummarizeGame <Game ID>
 	#
 	#
+def SummarizeGame(gameID):
+	print "sum game"
 #SummarizeVictory <Game ID> <Victory ID>
 	#
 	#
+def SummarizeVictory(gameID, victoryID):
+	print "sum victory"
 #VictoryRanking
 	#
 	#
+def VictoryRanking():
+	print "victory Ranking"
+def parse(elements):
+	element = elements.partition(" ")
+	if(element[0] is ""):
+		pass
+	else:
+		globals()[element[0]]
+	
+for line in fileinput.input():				# stdin, reads file line by line 
+    parse(line)
+
+# Trial and error stuff	
+# arr = {}
+# for i in range(0,10):
+		# arr[i] = i+1
+# print arr
+# func = "AddPlayer"
+# id = "1234"
+# name = "miguel"
+
+# methodToCall = globals()[func]
+# methodToCall(id,name)
+# methodToCall(1923,"luis")
