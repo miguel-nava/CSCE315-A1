19e704c0a5cb436b096442f5fc8e81873171a7b5
Miguel Nava <mike311878@compute-linux1.cs.tamu.edu>
2016-09-12 00:16:35 -0500

    added comments to the code as well as completed it

--- A1.py
+++ A1.py
@@ -1,43 +1,53 @@
 # Author: Miguel Nava
+# UIN:722006885
 # Assignment: Individual assignment: game leaderboard tracker
 import fileinput
+import operator
 
+######## Dictionaries ##############
 Player = { }
 PlayerPlays = {}
 PlayerFriends = {}
 PlayerVictories = {}
 Game = {}
 GameVictories = {}
+####################################
 
+
+######## Assignment Methods ########
 def AddPlayer (playerID, playerName): 
-    if str.isdigit(playerID): 		# checks if ID is an int
-        if playerID >= 0:					# checks if ID is positive 
-			if '\"' not in playerName:   			# checks for double quotes		
+    if str.isdigit(playerID): 						
+        if playerID >= 0:								 
+			if '\"' not in playerName:   					
 				Player[int(playerID)] = playerName
-				PlayerPlays[int(playerID)] = {}
-				PlayerVictories[int(playerID)] = {}
-	return
+				PlayerPlays[int(playerID)] = {}														# Initialization of dictionaries inside 
+				PlayerVictories[int(playerID)] = {}													# of dictionaries
 	
-#AddGame <Game ID> <Game Name>
 def AddGame (gameID, gameName):
-	Game[int(gameID)] = gameName
-	GameVictories[int(gameID)] = {}
+	if str.isdigit(gameID): 						
+		if (gameID >= 0):								 
+			if '\"' not in gameName:  
+				Game[int(gameID)] = gameName
+				GameVictories[int(gameID)] = {}														# Initialization of dictionary for victories
 
-#AddVictory <Game ID> <Victory ID> <Victory Name> <Victory Points>
 def AddVictory(gameID, victoryID, victoryName, victoryPoints):
-	GameVictories[int(gameID)][int(victoryID)] = (victoryName, int(victoryPoints) )
+	if str.isdigit(gameID) and str.isdigit(victoryID): 						
+		if int(gameID) >= 0 and int(victoryID) >= 0:								 
+			if '\"' not in victoryName and int(gameID) in Game:										# assures that the game exists 
+				GameVictories[int(gameID)][int(victoryID)] = (victoryName, int(victoryPoints) )
 	
-#Plays <Player ID> <Game ID> <Player IGN>
 def Plays(playerID, gameID, playerIGN):
-	if int(gameID) in PlayerPlays[int(playerID)]:
-		PlayerPlays[int(playerID)][int(gameID)].append(playerIGN)
-	else:
-		PlayerPlays[int(playerID)][int(gameID)] = [playerIGN]
+	if str.isdigit(gameID) and str.isdigit(playerID): 						
+		if int(gameID) >= 0 and int(playerID) >= 0:								 
+			if int(playerID) in Player and int(gameID) in Game:
+				if int(gameID) in PlayerPlays[int(playerID)]:
+					PlayerPlays[int(playerID)][int(gameID)].append(playerIGN)						# This method checks to see if a list of plays 
+				else:																				# for the player has been started. If it has,
+					PlayerPlays[int(playerID)][int(gameID)] = [playerIGN]							# then it appends, if not then it creates the list 
 	
-#AddFriends <Player ID1> <Player ID2>
 def AddFriends(playerID1, playerID2):
-	if int(playerID1)  in PlayerFriends:
-		PlayerFriends[int(playerID1)].append(int(playerID2))
+	if int(playerID1)  in PlayerFriends:															# Players are added to each others' friends list 
+		PlayerFriends[int(playerID1)].append(int(playerID2))										
 	else:
 		PlayerFriends[int(playerID1)] = [int(playerID2)]
 		
@@ -46,27 +56,25 @@ def AddFriends(playerID1, playerID2):
 	else:
 		PlayerFriends[int(playerID2)] = [int(playerID1)]
 	
-#WinVictory <Player ID> <Game ID> <Victory ID>
 def WinVictory(playerID, gameID, victoryID):
 	if int(gameID) in PlayerVictories[int(playerID)]:
 		PlayerVictories[int(playerID)][int(gameID)].append(int(victoryID))
 	else:
 		PlayerVictories[int(playerID)][int(gameID)] = [int(victoryID)]
-#FriendsWhoPlay <Player ID> <Game ID>
+
 def FriendsWhoPlay(playerID, gameID):
-	list = PlayerFriends[int(playerID)]
-	for i in list: 
-		if gameID in PlayerPlays[int(playerID)]:
+	list = PlayerFriends[int(playerID)]																# Receives all the player's friends from the list 
+	for i in list: 																					# amongst them it figures out if they have any similar 
+		if gameID in PlayerPlays[int(playerID)]:													# gameID's in order to determine if they play together or not 
 			if gameID in PlayerPlays[i]:
 				print Player[i] + " plays " + Game[int(gameID)] + " with " + Player[int(playerID)]
 
-#ComparePlayers <Player ID1> <Player ID2> <Game ID>
 def ComparePlayers(playerID1, playerID2, gameID):
-# Print report comparing player 1 and player 2's Victory records and total Victory scores for the given game. The given game is guaranteed to have been played by both players.
 	if gameID in PlayerPlays[int(playerID1)]:
 		if gameID in PlayerPlays[int(playerID2)]:
-			total = 0
-			print "\t" + Game[int(gameID)]
+			total = 0																				# Compares players on the basis of victories in a game 
+			print "\t" + Game[int(gameID)]															# and how many points they have in that game 
+			
 			print "---------------"
 			print Player[int(playerID1)]
 			for i in PlayerVictories[playerID1][gameID]:
@@ -82,10 +90,9 @@ def ComparePlayers(playerID1, playerID2, gameID):
 				print " " + GameVictories[int(gameID)][j][0]
 			print "Total points : " + `total`
 
-#SummarizePlayer <Player ID>
 def SummarizePlayer(playerID):
-	print "\t" + Player[int(playerID)]
-	
+	print "\t" + Player[int(playerID)]																# Gathers information about individual players such as 
+																									# friends, games and points from those games 
 	print "---------------"
 	print "Friends"
 	for i in PlayerFriends[int(playerID)]:
@@ -106,31 +113,77 @@ def SummarizePlayer(playerID):
 	print "---------------"
 	print "Total points"
 	print "  " + `total_points`
-#SummarizeGame <Game ID>
+
 def SummarizeGame(gameID):
-# Print a record of all players who play the specified game and the number of times each of its victories have been accomplished.
+	print "\t" + Game[int(gameID)]
 	
-	pass
+	print "---------------"
+	print "Players" 
+	for pID in PlayerPlays:																			#
+		if int(gameID) in PlayerPlays[pID]:
+			print "  " + Player[pID]
+	
+	print "---------------"
+	completed = 0 
+	print "Victories: Completed"
+	for vicID in GameVictories[int(gameID)]:
+		for pID in PlayerVictories:
+			if int(gameID) in PlayerVictories[pID]:
+				if vicID in PlayerVictories[pID][gameID]:
+					completed +=1
+		print "  " + GameVictories[int(gameID)][vicID][0] + ": " + `completed` 
+		completed = 0
 
-#SummarizeVictory <Game ID> <Victory ID>
 def SummarizeVictory(gameID, victoryID):
-# Print a list of all players who have achieved a Victory, and the percentage of players who play that game who have the Victory.
-	pass
-
-#VictoryRanking
-def VictoryRanking():
-# Print a summary ranking all players by their total number of gamer points.
-	pass
+	print "\tVictory: " + GameVictories[int(gameID)][int(victoryID)][0]
+	players_play = 0.0
+	players_win = 0.0
 	
+	print "---------------"
+	print "Players who have achieved victory"
+	for pID in PlayerVictories:
+		if int(gameID) in PlayerVictories[pID]:
+			if int(victoryID) in PlayerVictories[pID][int(gameID)]:
+				print "  " + Player[pID]															# Tracks players who have won and also the players who 
+				players_win += 1																	# have played the game 
+			players_play += 1
+			
+	print "---------------" 
+	print "Who won / Who played: " + `(players_win/players_play)*100` 
+
+def VictoryRanking():
+	list = {}
+	points = 0 
+	print "\tVictory Rankings"
+	
+	print "---------------" 
+	for pID in Player:																							
+		if pID in PlayerVictories:																	# Victory Ranking is done by calculating the players'
+			for gID in PlayerVictories[pID]:														# total points and putting them in a dictionary 
+				for vicID in PlayerVictories[pID][gID]:												# with their points as the key and their name is as 
+					points += GameVictories[gID][vicID][1]											# the information. 
+		list[points] = Player[pID]
+		points = 0 																					# The dictionary is then sorted from smallest to greatest 
+																									# based on the key and they are listed in descending order 
+	sorted_list = sorted(list.items(), key=operator.itemgetter(0))									# so that they come out with the highest scorer first 
+	count = 1
+	for i in range(len(sorted_list)-1,-1,-1):
+		print `count` + ". " + sorted_list[i][1] + " --- " + `sorted_list[i][0]`
+		count += 1		
+##################################	
+
+
+	
+######## Other Methods ###########
 def callMethod(m, a):
 	if len(a) == 0:
 		m();
-	elif len(a) == 1:
-		m(a[0])
-	elif len(a) == 2:
+	elif len(a) == 1:																				# This method is just to call a method based on different 
+		m(a[0])																						# argument amounts. m is the method to be called and 
+	elif len(a) == 2:																				# a is an array that holds the arguments in order 
 		m(a[0], a[1])
-	elif len(a) == 3:
-		m(a[0], a[1], a[2])
+	elif len(a) == 3:																				# We only go up to a = 4 because that the highest argument 
+		m(a[0], a[1], a[2])																			# function. a = 0 is the lowest 
 	elif len(a) == 4:
 		m(a[0], a[1], a[2], a[3])
 	
@@ -140,29 +193,23 @@ def parse(elements):
 	if(element[0] is ""):
 		pass
 	else:
-		method = globals()[element[0]]					# gets function
-		argNum = method.func_code.co_argcount			# gets number of args in the function
+		method = globals()[element[0]]																# gets function
+		argNum = method.func_code.co_argcount														# gets number of args in the function
 		for i in range(0,argNum-1):			
 			if ( "\"" not in element[2][0]):
-				element = element[2].partition(" ")
-				args[i] = element[0]
-			else:
+				element = element[2].partition(" ")													# partitioning is done according to the number of 
+				args[i] = element[0]																# arguments. Also it watches out for double quotes 
+			else:																					# in order to prevent seperation of the string 
 				element = element[2].split("\"")
 				args[i] = element[1]
 				element = element[2].partition(" ")
 		if ( "\"" not in element[2][0]):
-			args[argNum-1] = element[2]
-		else:
+			args[argNum-1] = element[2]																# doesn't partition the last element because it does not need to
+		else:																						# the last element is added at the end of the loop 
 			args[argNum-1] = element[2].split("\"")[1]
-		callMethod(method, args)
+		callMethod(method, args)																	# Once line is parsed, it calls the method by calling callMethod()
+##################################
+
 	
-for line in fileinput.input():				# stdin, reads file line by line 
-    parse(line)
-
-
-# print Player 
-# print Game
-# print GameVictories
-# print PlayerPlays
-# print PlayerVictories
-# print PlayerFriends
+for line in fileinput.input():																		# stdin, reads file line by line 
+    parse(line)																						# calls function to parse line 
